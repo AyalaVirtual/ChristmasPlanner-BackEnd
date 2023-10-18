be8c4d6e711be16da0e1ecc67a417776972fda86
AyalaVirtual <erica.ayala.work@gmail.com>
2023-10-18 19:55:17 -0400

    Initialize project and create README

---
+++ src/main/java/com/example/christmasplanner/ChristmasPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.example.christmasplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChristmasPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChristmasPlannerApplication.class, args);
+	}
+
+}
