ab23d017d28b005c75addd0556120aae6e08be8d
AyalaVirtual <erica.ayala.work@gmail.com>
2023-10-18 20:35:45 -0400

    Complete StockingStuffer model

--- src/main/java/com/example/christmasplanner/model/StockingStuffer.java
+++ src/main/java/com/example/christmasplanner/model/StockingStuffer.java
@@ -1,4 +1,65 @@
 package com.example.christmasplanner.model;
 
+import javax.persistence.*;
+
+
+@Entity
+@Table(name="stockingstuffers")
 public class StockingStuffer {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String description;
+
+
+    public StockingStuffer() {
+    }
+
+    public StockingStuffer(Long id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+    @Override
+    public String toString() {
+        return "StockingStuffer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
