c877b9eac16143c3ab798def6423342a718c629a
AyalaVirtual <erica.ayala.work@gmail.com>
2023-10-19 11:37:42 -0400

    Create GET by id method for gift model in service class

--- src/main/java/com/example/christmasplanner/service/GiftService.java
+++ src/main/java/com/example/christmasplanner/service/GiftService.java
@@ -1,10 +1,12 @@
 package com.example.christmasplanner.service;
 
+import com.example.christmasplanner.exception.InformationNotFoundException;
 import com.example.christmasplanner.model.Gift;
 import com.example.christmasplanner.repository.GiftRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -21,6 +23,16 @@ public class GiftService {
 
     public List<Gift> getAllGifts() {
         return giftRepository.findAll();
+    }
+
+    public Optional<Gift> getGiftById(Long giftId) {
+        Optional<Gift> giftOptional = giftRepository.findById(giftId);
+
+        if (giftOptional.isPresent()) {
+            return giftOptional;
+        } else {
+            throw new InformationNotFoundException("gift with id " + giftId + " not found");
+        }
     }
 
 
