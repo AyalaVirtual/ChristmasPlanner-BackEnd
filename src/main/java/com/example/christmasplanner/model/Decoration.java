c2fd4389f57b1e1dcdc9955fa965b578f755e142
AyalaVirtual <erica.ayala.work@gmail.com>
2023-10-18 20:27:53 -0400

    Edit README to reflect change in user stories section

--- src/main/java/com/example/christmasplanner/model/Decoration.java
+++ src/main/java/com/example/christmasplanner/model/Decoration.java
@@ -1,4 +1,25 @@
 package com.example.christmasplanner.model;
 
+import javax.persistence.*;
+
+
+@Entity
+@Table(name="decorations")
 public class Decoration {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String materials;
+
+    @Column
+    private String directions;
+
+
 }
