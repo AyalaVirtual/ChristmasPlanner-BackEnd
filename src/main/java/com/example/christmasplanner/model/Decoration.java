9181c8b197f157c9bc2325a19713d6c882ffd5fc
AyalaVirtual <erica.ayala.work@gmail.com>
2023-10-18 20:30:36 -0400

    Complete Decoration model

--- src/main/java/com/example/christmasplanner/model/Decoration.java
+++ src/main/java/com/example/christmasplanner/model/Decoration.java
@@ -22,4 +22,57 @@ public class Decoration {
     private String directions;
 
 
+    public Decoration() {
+    }
+
+    public Decoration(Long id, String name, String materials, String directions) {
+        this.id = id;
+        this.name = name;
+        this.materials = materials;
+        this.directions = directions;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMaterials() {
+        return materials;
+    }
+
+    public void setMaterials(String materials) {
+        this.materials = materials;
+    }
+
+    public String getDirections() {
+        return directions;
+    }
+
+    public void setDirections(String directions) {
+        this.directions = directions;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Decoration{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", materials='" + materials + '\'' +
+                ", directions='" + directions + '\'' +
+                '}';
+    }
 }
