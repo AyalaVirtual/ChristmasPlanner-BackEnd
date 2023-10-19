dd6b08cb6e8e60272b882ffc292ff4ac67360ab2
AyalaVirtual <erica.ayala.work@gmail.com>
2023-10-18 20:33:07 -0400

    Complete Gift model

--- src/main/java/com/example/christmasplanner/model/Gift.java
+++ src/main/java/com/example/christmasplanner/model/Gift.java
@@ -1,4 +1,65 @@
 package com.example.christmasplanner.model;
 
+import javax.persistence.*;
+
+
+@Entity
+@Table(name="gifts")
 public class Gift {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String description;
+
+
+    public Gift() {
+    }
+
+    public Gift(Long id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Gift{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
