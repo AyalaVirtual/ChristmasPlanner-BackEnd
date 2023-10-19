695b67c3a20b075816839285dcc2f68f91097429
AyalaVirtual <erica.ayala.work@gmail.com>
2023-10-19 13:37:43 -0400

    Add GET by id method for stocking stuffers to controller.

--- src/main/java/com/example/christmasplanner/controller/StockingStufferController.java
+++ src/main/java/com/example/christmasplanner/controller/StockingStufferController.java
@@ -6,11 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 
 
 @RestController
@@ -40,6 +41,20 @@ public class StockingStufferController {
             message.put("message", "success");
             message.put("data", stockingStufferList);
             return new ResponseEntity<>(message, HttpStatus.OK);
+        }
+    }
+
+    @GetMapping(path="/stockingstuffers/{stockingStufferId}/")
+    public ResponseEntity<?> getStockingStufferById(@PathVariable(value="stockingStufferId") Long stockingStufferId) {
+        Optional<StockingStuffer> stockingStufferOptional = stockingStufferService.getStockingStufferById(stockingStufferId);
+
+        if (stockingStufferOptional.isPresent()) {
+            message.put("message", "success");
+            message.put("data", stockingStufferOptional);
+            return new ResponseEntity<>(message, HttpStatus.OK);
+        } else {
+            message.put("message", "stocking stuffer with id " + stockingStufferId + " not found");
+            return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
         }
     }
 
