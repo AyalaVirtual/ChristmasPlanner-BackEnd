0e8f5c23e83d248f621be8ac7a60c6523c7f77f2
AyalaVirtual <erica.ayala.work@gmail.com>
2023-10-18 21:09:56 -0400

    Refactor GET all method in Decoration controller to reflect HTTP response and status.

--- src/main/java/com/example/christmasplanner/controller/DecorationController.java
+++ src/main/java/com/example/christmasplanner/controller/DecorationController.java
@@ -3,9 +3,14 @@ package com.example.christmasplanner.controller;
 import com.example.christmasplanner.model.Decoration;
 import com.example.christmasplanner.service.DecorationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 
@@ -15,6 +20,9 @@ public class DecorationController {
 
     private DecorationService decorationService;
 
+    static HashMap<String, Object> result = new HashMap<>();
+    static HashMap<String, Object> message = new HashMap<>();
+
 
     @Autowired
     public void setDecorationService(DecorationService decorationService) {
@@ -23,9 +31,20 @@ public class DecorationController {
 
 
     @GetMapping(path="/decorations/")
-    public List<Decoration> getAllDecorations() {
-        return decorationService.getAllDecorations();
+    public ResponseEntity<?> getAllDecorations() {
+        List<Decoration> decorationList = decorationService.getAllDecorations();
+
+        if (decorationList.isEmpty()) {
+            message.put("message", "cannot find any decorations");
+            return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+        } else {
+            message.put("message", "success");
+            message.put("data", decorationList);
+            return new ResponseEntity<>(message, HttpStatus.OK);
+        }
     }
+
+
 
 
 }
