ba74677286cdf60d838a5ab45e522a69013959ec
AyalaVirtual <erica.ayala.work@gmail.com>
2023-10-19 15:37:51 -0400

    Create test runner

---
+++ src/test/java/runners/TestRunner.java
@@ -0,0 +1,18 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+// This says our testing is going to be run with the Cucumber class
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "definitions",
+        plugin = {"pretty"},
+        // This means when we run our test, Cucumber is going to check all the steps inside the features file and check that we have them written inside the TestDefinitions.java
+        dryRun = true
+)
+public class TestRunner {
+}
