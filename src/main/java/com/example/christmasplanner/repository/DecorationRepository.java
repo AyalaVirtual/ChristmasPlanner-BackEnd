04820faf8233f3e969a7a0511a3ccd9d1b89a97f
AyalaVirtual <erica.ayala.work@gmail.com>
2023-10-18 20:43:50 -0400

    Create repository for Decoration model

---
+++ src/main/java/com/example/christmasplanner/repository/DecorationRepository.java
@@ -0,0 +1,16 @@
+package com.example.christmasplanner.repository;
+
+import com.example.christmasplanner.model.Decoration;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface DecorationRepository extends JpaRepository<Decoration, Long> {
+
+    Optional<Decoration> findById(Long decorationId);
+
+    Optional<Decoration> findByName(String decorationName);
+}
