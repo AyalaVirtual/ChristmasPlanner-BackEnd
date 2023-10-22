89a3854e47bfb62350eab3202f7e372ecb967986
AyalaVirtual <erica.ayala.work@gmail.com>
2023-10-21 20:47:00 -0400

    Edit comments and add doc string in Cors Config class.

--- src/main/java/com/example/christmasplanner/CorsConfig.java
+++ src/main/java/com/example/christmasplanner/CorsConfig.java
@@ -9,10 +9,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
+    // Configures Cross-Origin Resource Sharing (CORS) settings to enable cross-origin requests, making the API accessible to the Angular front-end.
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
-            // This enables cross-origin requests and makes the API accessible to the Angular front-end
+
+            /**
+             * Defines CORS policies, allowing specific HTTP methods, headers, and origins,
+             * and specifying exposed headers for cross-origin requests.
+             *
+             * @param registry represents the CorsRegistry for configuring CORS rules.
+             */
             @Override
             public void addCorsMappings(CorsRegistry registry) {
 
